cc890ca0a4e9810f0679c9376b4cf0d03276b423
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2021-11-30 22:37:50 -0300

    Correção do código

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -1,38 +1,39 @@
-// Salvar a URL do back-end em uma variavel para ser utilizada depois na hora da chamada da api
+// Salvar a url do backend em uma variavel para ser ultiliza depois na hora da chamada da api
 const apiUrl = "http://localhost:3000";
 
-/// estou mapeando o elemento lista (<table></table>) do html.
+// Estou mapeando o elemento lista (<table></table>) do html.
 const lista = document.getElementById("lista");
 
-// Crio um função onde é possivel realizar uma requisição [GET] para a api
-const getVagas = () => {
-  // FETCH - É usado para se comunicar via requisição http (GET, POST, PUT, PACH, GELETE);
-  // Response - é a resposta se a chamada da api foi feita com sucesso (status 200);
-  // FETCH quando não passada configuração e apenas a url ele faz uma chamada do tipo [GET];
+// Crio uma funcao onde é possivel realizar uma requisicao [GET] para a api
+const getVagas = async () => {
+  //  FETCH - É usado para se comunicar via requisicao http (GET, POST, PUT, PATCH, DELETE);
+  //  Response - é a resposta se a chamada da api foi feita com sucesso (status 200);
+  // FETCH quando nao passada configuracao e apenas a url ele faz uma chamada do tipo [GET];
+  // const chamadaApi = fetch(`${apiUrl}/vagas`)
+  // chamadaApi.then((response) => {
+  //     console.log('RESPOSTA REQUISICAO', response);
+  //     return response.json();
+  // })
+  // .then((vagas) => {
+  //     console.log('RESPOSTA DADOS JSON', vagas);
+  // })
 
-  //   const chamadaApi = fetch("${apiUrl}/vagas");
-  //   chamadaApi.then((response) => {
-  //     console.log("RESPOSTA REQUISICAO", response);
-  //     return response.json;
-  //   });
-  //   .then((vagas)=>{
-  //       console.log("RESPOSTA DADOS JSON",vagas);
-  //   });
-
-  const response = await fetch("${apiUrl}/vagas");
+  const response = await fetch(`${apiUrl}/vagas`);
   const vagas = await response.json();
+
   vagas.map((vaga) => {
+    console.log(vaga.empresa);
     lista.insertAdjacentHTML(
       "beforeend",
       `
-    <tr>
-        <td>${vaga.id}</td>
-        <td>${vaga.empresa}</td>
-        <td>${vaga.oportunidade}</td>
-        <td>${vaga.tipo}/td>
-        <td>${vaga.salario}</td>
-    </tr>
-  `
+            <tr>
+                <td>${vaga.id}</td>
+                <td>${vaga.empresa}</td>
+                <td>${vaga.oportunidade}</td>
+                <td>${vaga.tipo}</td>
+                <td>${vaga.salario}</td>
+            </tr>
+        `
     );
   });
 };
@@ -40,24 +41,24 @@ const getVagas = () => {
 getVagas();
 
 const escolherVaga = async () => {
-  // Buscando o que o usuário digitou no input
+  // Buscando o que o usuario digitou no input
   const idDigitado = document.getElementById("idVaga").value;
-  // fazendo a chamada para a api /vagas/{id} para pegar a vaga individual
+  // Fazendo a chamdada para a api /vagas/{id} para pegar a vaga individual
   const response = await fetch(`${apiUrl}/vagas/${idDigitado}`);
-  //Salva o objeto retornado pelo back-end
+  // Salvo o objeto retornado pelo backend;
   const vaga = await response.json();
 
-  // mapeando a tabela do html e inserindo uma vaga dentro
+  // Mapeando a tabela do html e inserindo uma vaga dentro
   document.getElementById("vaga").insertAdjacentHTML(
     "beforeend",
     `
-  <tr>
-  <td>${vaga.id}</td>
-  <td>${vaga.empresa}</td>
-  <td>${vaga.oportunidade}</td>
-  <td>${vaga.tipo}</td>
-  <td>${vaga.salario}</td>
-</tr>
-`
+        <tr>
+            <td>${vaga.id}</td>
+            <td>${vaga.empresa}</td>
+            <td>${vaga.oportunidade}</td>
+            <td>${vaga.tipo}</td>
+            <td>${vaga.salario}</td>
+        </tr>
+    `
   );
 };
